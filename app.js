00afe2a223a6c96c7c5c985bb5dcec9cc28f6e05
GeEuzebio <georgeeuzebio@gmail.com>
2023-10-30 14:17:14 -0300

    criando back-end

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 5000;
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+    res.send('Hello Wolrd');
+});
+
+app.listen(port, () => {
+    console.log('Servidor rodando em http://localhost:5000');
+});
